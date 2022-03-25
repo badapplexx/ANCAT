e6c570d0f817d036a0727e5c7ecced51f682cee7
badapplexx <ipek.peskirci@gmail.com>
2022-03-25 21:43:12 +0300

    Message set(end system info is done!!)
    messageCount is missing. Will be added

--- main.py
+++ main.py
@@ -14,20 +14,6 @@ sheet2Name = "Settings"
 sheet2_column1Name = "Setting"
 sheet2_column2Name = "Value"
 sheet3Name = "End Systems"
-Sheet3_column1 = "End Systems"
-Sheet3_column2 = "Source ID"
-Sheet3_column3 = "Datarate of source"
-Sheet3_column4 = "Cable Length"
-Sheet3_column5 = "VLID"
-Sheet3_column6 = "startTime"
-Sheet3_column7 = "stopTime"
-Sheet3_column8 = "BAG"
-Sheet3_column9 = "Payload Length"
-Sheet3_column10 = "Delta Payload Length"
-Sheet3_column11 = "Period"
-Sheet3_column12 = "Delta Period"
-Sheet3_column13 = "rho"
-Sheet3_column14 = "sigma"
 
 iniFileName = "Network.ini"
 endSystemIndicator = "ES"
@@ -35,32 +21,19 @@ switchIndicator = "SW"
 networkName = "AFDXNetwork"
 
 # SOME DECLARATIONS
-# flags for optional(?) ini lines
-createConTypeLines = True
+
 iniString1Header = ""
 iniString2GenNetworkParams = ""
-iniString5EthernetSettings = ""
-iniString4Conndef = ""
-iniString6ESGeneral_numberOfs = ""
-iniString8ESGeneral_skewMax = ""
-iniString9ESGeneralVLQueueLength = ""
-iniString10ESGeneral_defaults = ""
-iniString7ESGeneral_TechDelays = ""
-
-iniStringES_MessageCounts = ""
-iniStringES_Datarates = ""
-iniStringES_CableLengths = ""
-iniStringES_VLIDs = ""
-iniStringES_StartTimes = ""
-iniStringES_StopTimes = ""
-iniStringES_BAGs = ""
-iniStringES_Payloads = ""
-iniStringES_PayloadDeltas = ""
-iniStringES_Rho = ""
-iniStringES_Sigma = ""
+iniString3Conndef = ""
+iniString4EthernetSettings = ""
+iniString5ESGeneral_numberOfs = ""
+iniString6ESGeneral_TechDelays = ""
+iniString7ESGeneral_skewMax = ""
+iniString8ESGeneralVLQueueLength = ""
+iniString9ESGeneral_defaults = ""
 
 
-# get excel content for sheet: "Topology" and "Settings"
+# get excel content
 with pd.ExcelFile(inputFileName) as file:
     sheet1 = pd.read_excel(file, sheet1Name)
     sheet2 = pd.read_excel(file, sheet2Name)
@@ -69,21 +42,23 @@ with pd.ExcelFile(inputFileName) as file:
 # get column one(for entry nodes) and column two(for exit nodes)
 entryList = sheet1[sheet1_column1Name].values.tolist()
 exitsList = sheet1[sheet1_column2Name].values.tolist()
-print(entryList)
-print(exitsList)
-# get settings
+#print(entryList)
+#print(exitsList)
+
+## SETTINGS
 settingNamesList = sheet2[sheet2_column1Name].values.tolist()
 settingValuesList = sheet2[sheet2_column2Name].values.tolist()
 
-switchTechDelay = settingValuesList[settingNamesList.index("Switch Tech Delay")]  # "4us"
-switchSchServiceTime = "0"
-ESRxTechDelay = settingValuesList[settingNamesList.index("ES Rx Tech Delay")]  # "32ms"
-ESTxTechDelay = settingValuesList[settingNamesList.index("ES Tx Tech Delay")]  # "32ms"
-ethernetSpeed = settingValuesList[settingNamesList.index("Ethernet Speed")]  # "100Mbps"
-ethernetCableLength = settingValuesList[settingNamesList.index("Ethernet Cable Length")]  # "10m"
-skewMax = settingValuesList[settingNamesList.index("Skew Max")]  # "10ms"
-vlQueueLength = settingValuesList[settingNamesList.index("VL Queue size")]  # "1000"
+switchTechDelay = settingValuesList[settingNamesList.index("Switch Tech Delay")]
+ESRxTechDelay = settingValuesList[settingNamesList.index("ES Rx Tech Delay")]
+ESTxTechDelay = settingValuesList[settingNamesList.index("ES Tx Tech Delay")]
+ethernetSpeed = settingValuesList[settingNamesList.index("Ethernet Speed")]
+ethernetCableLength = settingValuesList[settingNamesList.index("Ethernet Cable Length")]
+skewMax = settingValuesList[settingNamesList.index("Skew Max")]
+vlQueueLength = settingValuesList[settingNamesList.index("VL Queue size")]
+
 # Parameters to be filled with default values
+switchSchServiceTime = "0"
 def_isSkewMaxTestEnabled = "false"
 def_networkID = "0x99"
 def_interfaceID = "0"
@@ -95,8 +70,100 @@ def_frameHeaderLength = "47"
 # Get message sets vs
 endOfFile = sheet3[sheet3.isnull().all(axis=1) == True].index.tolist()[0] # get final line number
 headerList = sheet3.columns.tolist()
-iniStringMessageSet = [" "]*endOfFile
 
+############################# PART2 #############################
+##################### CREATE and FILL *.ini #####################
+### Header ###
+iniString1Header = "[General]\n" "**.vector-recording = true\n" "**.scalar-recording = true"
+#print(iniString1Header + "\n")
+
+### Network Parameters ###
+iniString2GenNetworkParams = "#Network Params\n"  f"network = {networkName}"
+#print(iniString2GenNetworkParams + "\n")
+
+
+# Connection definitions: ex. ES1<->SW1 ..
+connDefEntryTypeString = "*.connDef[{}].isEntryAnEndsystem = {}"
+connDefExitTypeString = "*.connDef[{}].isExitAnEndsystem = {}"
+connDefEntryIndexString = "*.connDef[{}].entryIndex = {}"
+connDefExitIndexString = "*.connDef[{}].exitIndex = {}"
+
+# initialize Parameters
+ESSet = set()
+SWSet = set()
+connDefEntryTypeStringList = []
+connDefExitTypeStringList = []
+connDefEntryIndexStringList = []
+connDefExitIndexStringList = []
+
+# add values(ESn, SWn) in the column to a set to count unique occurrences
+# create appropriate lines for *.ini file
+i = 0
+for s in entryList:
+    connDefEntryIndexStringList.append(connDefEntryIndexString.format(i, s[2:4]))
+    if endSystemIndicator in s:
+        ESSet.add(s)
+        connDefEntryTypeStringList.append(connDefEntryTypeString.format(i, "true"))
+    elif switchIndicator in s:
+        SWSet.add(s)
+        connDefEntryTypeStringList.append(connDefEntryTypeString.format(i, "false"))
+    i += 1
+
+i = 0
+for s in exitsList:
+    connDefExitIndexStringList.append(connDefExitIndexString.format(i, s[2:4]))
+    if endSystemIndicator in s:
+        ESSet.add(s)
+        connDefExitTypeStringList.append(connDefExitTypeString.format(i, "true"))
+    elif switchIndicator in s:
+        SWSet.add(s)
+        connDefExitTypeStringList.append(connDefExitTypeString.format(i, "false"))
+    i += 1
+
+numberOfES = len(ESSet)
+numberOfSW = len(SWSet)
+
+for s in connDefEntryIndexStringList:
+    iniString3Conndef += s + "\n"
+iniString3Conndef += "\n"
+for s in connDefEntryTypeStringList:
+    iniString3Conndef += s + "\n"
+iniString3Conndef += "\n"
+for s in connDefExitIndexStringList:
+    iniString3Conndef += s + "\n"
+iniString3Conndef += "\n"
+for s in connDefExitTypeStringList:
+    iniString3Conndef += s + "\n"
+
+#print(iniString3Conndef + "\n")
+
+iniString4EthernetSettings = f"*.ethSpeed = {ethernetSpeed}\n*.cableLength = {ethernetCableLength}"
+iniString5ESGeneral_numberOfs = f"**.numberOfEndSystems = {numberOfES}\n" \
+                                f"**.numberOfSwitches = {numberOfSW}"
+iniString6ESGeneral_TechDelays = f"**.scheduler.serviceTime = {switchSchServiceTime}\n"\
+                                 f"**.switchFabric.delay.delay = {switchTechDelay}\n" \
+                                 f"**.ESGroup[*].latencyTechTx.delay = {ESTxTechDelay}\n" \
+                                 f"**.ESGroup[*].latencyTechRx.delay = {ESRxTechDelay}"
+iniString7ESGeneral_skewMax = f"**.redundancyChecker.skewMax = {skewMax}"
+iniString8ESGeneralVLQueueLength = f"**.regulatorLogic.maxVLIDQueueSize = {vlQueueLength}"
+#print(iniString4EthernetSettings)
+#print(iniString5ESGeneral_numberOfs)
+#print(iniString6ESGeneral_TechDelays)
+#print(iniString7ESGeneral_skewMax)
+#print(iniString8ESGeneralVLQueueLength + "\n")
+
+iniString9ESGeneral_defaults = f"**.afdxMarshall[*].networkId = {def_networkID}\n"
+iniString9ESGeneral_defaults += f"**.afdxMarshall[*].interfaceId = {def_interfaceID}\n"
+iniString9ESGeneral_defaults += f"**.afdxMarshall[*].seqNum = {def_seqNum}\n"
+iniString9ESGeneral_defaults += f"**.afdxMarshall[*].udpSrcPort = {def_udpSrcPort}\n"
+iniString9ESGeneral_defaults += f"**.afdxMarshall[*].udpDestPort = {def_udpDestPort}\n"
+iniString9ESGeneral_defaults += f"**.afdxMarshall[*].frameHeaderLength = {def_frameHeaderLength}\n"
+iniString9ESGeneral_defaults += f"**.skewMaxTester.skewMaxTestEnabled = {def_isSkewMaxTestEnabled}"
+#print(iniString9ESGeneral_defaults + "\n")
+
+
+# Per end system parameters
+iniStringMessageSet = [" "]*endOfFile
 for rowIndex in range(endOfFile):
     esIndex = 0
     sourceIndex = 0
@@ -104,7 +171,7 @@ for rowIndex in range(endOfFile):
     for colIndex in range(len(headerList)):
         if colIndex == 0:  # col: End System
             esName = row[colIndex]
-            esIndex = esName[2:3]
+            esIndex = esName[2:4]
         if colIndex == 1:  # col: Source ID
             sourceIndex = row[colIndex]
         if colIndex == 2:  # col: Datarate of Source
@@ -131,98 +198,26 @@ for rowIndex in range(endOfFile):
             iniStringMessageSet[rowIndex] += f"**.ESGroup[{esIndex}].trafficSource[{sourceIndex}].rho = {row[colIndex]} "
         if colIndex == 13:  # col: sigma
             iniStringMessageSet[rowIndex] += f"**.ESGroup[{esIndex}].trafficSource[{sourceIndex}].sigma = {row[colIndex]} "
-    print(iniStringMessageSet[rowIndex])
-print("over")
-############################# PART2 #############################
-##################### CREATE and FILL *.ini #####################
-### Header ###
-iniString1Header = "[General]\n" "**.vector-recording = true\n" "**.scalar-recording = true"
-print(iniString1Header + "\n")
-
-### Network Parameters ###
-iniString2GenNetworkParams = "#Network Params\n"  f"network = {networkName}"
-print(iniString2GenNetworkParams + "\n")
+        iniStringMessageSet[rowIndex] += "\n"
+    #print(iniStringMessageSet[rowIndex])
 
 
-# Connection definitions: ex. ES1<->SW1 ..
-connDefEntryTypeString = "*.connDef[{}].isEntryAnEndsystem = {}"
-connDefExitTypeString = "*.connDef[{}].isExitAnEndsystem = {}"
-connDefEntryIndexString = "*.connDef[{}].entryIndex = {}"
-connDefExitIndexString = "*.connDef[{}].exitIndex = {}"
-
-# initialize Parameters
-ESSet = set()
-SWSet = set()
-connDefEntryTypeStringList = []
-connDefExitTypeStringList = []
-connDefEntryIndexStringList = []
-connDefExitIndexStringList = []
-
-# add values(ESn, SWn) in the column to a set to count unique occurrences
-# create appropriate lines for *.ini file
-i = 0
-for s in entryList:
-    connDefEntryIndexStringList.append(connDefEntryIndexString.format(i, s[2:3]))
-    if endSystemIndicator in s:
-        ESSet.add(s)
-        connDefEntryTypeStringList.append(connDefEntryTypeString.format(i, "true"))
-    elif switchIndicator in s:
-        SWSet.add(s)
-        connDefEntryTypeStringList.append(connDefEntryTypeString.format(i, "false"))
-    i += 1
-
-i = 0
-for s in exitsList:
-    connDefExitIndexStringList.append(connDefExitIndexString.format(i, s[2:3]))
-    if endSystemIndicator in s:
-        ESSet.add(s)
-        connDefExitTypeStringList.append(connDefExitTypeString.format(i, "true"))
-    elif switchIndicator in s:
-        SWSet.add(s)
-        connDefExitTypeStringList.append(connDefExitTypeString.format(i, "false"))
-    i += 1
-
-numberOfES = len(ESSet)
-numberOfSW = len(SWSet)
-
-for s in connDefEntryIndexStringList:
-    iniString4Conndef += s + "\n"
-iniString4Conndef += "\n"
-for s in connDefEntryTypeStringList:
-    iniString4Conndef += s + "\n"
-iniString4Conndef += "\n"
-for s in connDefExitIndexStringList:
-    iniString4Conndef += s + "\n"
-iniString4Conndef += "\n"
-for s in connDefExitTypeStringList:
-    iniString4Conndef += s + "\n"
-
-print(iniString4Conndef + "\n")
-
-iniString5EthernetSettings = f"*.ethSpeed = {ethernetSpeed}\n*.cableLength = {ethernetCableLength}"
-iniString6ESGeneral_numberOfs = f"**.numberOfEndSystems = {numberOfES}\n" \
-                                f"**.numberOfSwitches = {numberOfSW}"
-iniString7ESGeneral_TechDelays = f"**.scheduler.serviceTime = {switchSchServiceTime}\n"\
-                                 f"**.switchFabric.delay.delay = {switchTechDelay}\n" \
-                                 f"**.ESGroup[*].latencyTechTx.delay = {ESTxTechDelay}\n" \
-                                 f"**.ESGroup[*].latencyTechRx.delay = {ESRxTechDelay}"
-iniString8ESGeneral_skewMax = f"**.redundancyChecker.skewMax = {skewMax}"
-iniString9ESGeneralVLQueueLength = f"**.regulatorLogic.maxVLIDQueueSize = {vlQueueLength}"
-print(iniString5EthernetSettings)
-print(iniString6ESGeneral_numberOfs)
-print(iniString7ESGeneral_TechDelays)
-print(iniString8ESGeneral_skewMax)
-print(iniString9ESGeneralVLQueueLength + "\n")
-
-iniString10ESGeneral_defaults = f"**.afdxMarshall[*].networkId = {def_networkID}\n"
-iniString10ESGeneral_defaults += f"**.afdxMarshall[*].interfaceId = {def_interfaceID}\n"
-iniString10ESGeneral_defaults += f"**.afdxMarshall[*].seqNum = {def_seqNum}\n"
-iniString10ESGeneral_defaults += f"**.afdxMarshall[*].udpSrcPort = {def_udpSrcPort}\n"
-iniString10ESGeneral_defaults += f"**.afdxMarshall[*].udpDestPort = {def_udpDestPort}\n"
-iniString10ESGeneral_defaults += f"**.afdxMarshall[*].frameHeaderLength = {def_frameHeaderLength}\n"
-iniString10ESGeneral_defaults += f"**.skewMaxTester.skewMaxTestEnabled = {def_isSkewMaxTestEnabled}\n"
-print(iniString10ESGeneral_defaults)
-# Per trafficSource parameters
+# Open a file with access mode 'a'
+iniFile = open(iniFileName, 'w')
+# Append 'hello' at the end of file
+iniFile.write(iniString1Header + "\n\n")
+iniFile.write(iniString2GenNetworkParams + "\n\n")
+iniFile.write(iniString3Conndef + "\n\n")
+iniFile.write(iniString4EthernetSettings + "\n\n")
+iniFile.write(iniString5ESGeneral_numberOfs + "\n")
+iniFile.write(iniString7ESGeneral_skewMax + "\n")
+iniFile.write(iniString8ESGeneralVLQueueLength + "\n")
+iniFile.write(iniString6ESGeneral_TechDelays + "\n")
+iniFile.write(iniString9ESGeneral_defaults + "\n")
+for str in iniStringMessageSet:
+    iniFile.write(str)
+# Close the file
+iniFile.close()
 
 
 
