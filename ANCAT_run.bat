792bddb31272d3788eb28538a70489f5014f3e3c
badapplexx <ipek.peskirci@gmail.com>
2022-04-23 14:51:45 +0300

    ANCAT_main's functions are added to ANCAT_run.bat

--- ANCAT_run.bat
+++ ANCAT_run.bat
@@ -1,3 +1,8 @@
-@echo off
-python ANCAT_main.py -omnetPath C:\omnetpp-5.7 -afdxPath C:\omnetpp-5.7\samples\AFDX-Master-git -xlsx C:\Users\ozerg\Desktop\Share\ANCAT\input_data\Exp2\Config_Exp2.xlsx -reportPath C:\Users\ozerg\Desktop -reportName exp2 -keepFig
+python PreProcessor.py -i D:\Documents\docs-tez-depo\ThesisWork\ANCAT\Exp1\12\results\Config_Exp12.xlsx -o D:\Documents\docs-tez-depo\ThesisWork\ANCAT\Exp1\12\omnetpp
+python PreProcessor.py -i D:\Documents\docs-tez-depo\ThesisWork\ANCAT\Exp1\12\results\Config_Exp12.xlsx -o C:\Workspaces\Github\AFDX\simulations
+python SimProcessor.py -omnetPath C:\omnetpp-5.6.2 -afdxPath C:\Workspaces\Github\AFDX 
+PAUSE
+copy C:\Workspaces\Github\AFDX\simulations\results\General-#0.vec D:\Documents\docs-tez-depo\ThesisWork\ANCAT\Exp1\12\omnetpp
+copy C:\Workspaces\Github\AFDX\simulations\results\General-#0.vci D:\Documents\docs-tez-depo\ThesisWork\ANCAT\Exp1\12\omnetpp
+python PostProcessor.py -iPath D:\Documents\docs-tez-depo\ThesisWork\ANCAT\Exp1\12\omnetpp -oPath D:\Documents\docs-tez-depo\ThesisWork\ANCAT\Exp1\12\results -oFile exp12
 PAUSE
