e81a604087c9c16d3489769379d45be96e118673
badapplexx <ipek.peskirci@gmail.com>
2022-04-25 22:19:40 +0300

    path place holders are added

--- ANCAT_run.bat
+++ ANCAT_run.bat
@@ -1,7 +1,11 @@
-python PreProcessor.py -i D:\Documents\docs-tez-depo\ThesisWork\ANCAT\Exp1\12\results\Config_Exp12.xlsx -o D:\Documents\docs-tez-depo\ThesisWork\ANCAT\Exp1\12\omnetpp
-python PreProcessor.py -i D:\Documents\docs-tez-depo\ThesisWork\ANCAT\Exp1\12\results\Config_Exp12.xlsx -o C:\Workspaces\Github\AFDX\simulations
-python SimProcessor.py -omnetPath C:\omnetpp-5.6.2 -afdxPath C:\Workspaces\Github\AFDX 
-copy C:\Workspaces\Github\AFDX\simulations\results\General-#0.vec D:\Documents\docs-tez-depo\ThesisWork\ANCAT\Exp1\12\omnetpp
-copy C:\Workspaces\Github\AFDX\simulations\results\General-#0.vci D:\Documents\docs-tez-depo\ThesisWork\ANCAT\Exp1\12\omnetpp
-python PostProcessor.py -iPath D:\Documents\docs-tez-depo\ThesisWork\ANCAT\Exp1\12\omnetpp -oPath D:\Documents\docs-tez-depo\ThesisWork\ANCAT\Exp1\12\results -oFile exp12
+set OMNET_PATH=C:\omnetpp-5.7
+set AFDX_PATH=C:\omnetpp-5.7\samples\AFDX-Master-git
+set XLSX_PATH=C:\Users\ozerg\Desktop\Share\ANCAT\input_data\Exp20
+set XLSX_NAME=Config.xlsx
+set REPORT_PATH=C:\Users\ozerg\Desktop
+set REPORT_NAME=exp20
+
+PreProcessor.exe -i %XLSX_PATH%\%XLSX_NAME% -o %AFDX_PATH%\simulations
+SimProcessor.exe -omnetPath %OMNET_PATH% -afdxPath %AFDX_PATH% 
+PostProcessor.exe -iPath %AFDX_PATH%\simulations\results -oPath %REPORT_PATH% -oFile %REPORT_NAME%
 PAUSE
