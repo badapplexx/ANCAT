3e0fe5e072f2007c65166e024d5839ef32da29d5
badapplexx <ipek.peskirci@gmail.com>
2022-05-19 15:21:33 +0300

    Updated to omnetpp6

--- ANCAT_run.bat
+++ ANCAT_run.bat
@@ -1,11 +1,11 @@
-set OMNET_PATH=C:\omnetpp-5.7
-set AFDX_PATH=C:\omnetpp-5.7\samples\AFDX-Master-git
-set XLSX_PATH=C:\Users\ozerg\Desktop\Share\ANCAT\input_data\Exp20
-set XLSX_NAME=Config.xlsx
-set REPORT_PATH=C:\Users\ozerg\Desktop
-set REPORT_NAME=exp20
+set OMNET_PATH=C:\omnetpp-6.0
+set AFDX_PATH=C:\Workspaces\Github\AFDX_6
+set XLSX_PATH=C:\Workspaces\Github\ANCAT
+set XLSX_NAME=Config_Exp2.xlsx
+set REPORT_PATH=C:\Workspaces\Github\ANCAT
+set REPORT_NAME=exp2
 
-PreProcessor.exe -i %XLSX_PATH%\%XLSX_NAME% -o %AFDX_PATH%\simulations
-SimProcessor.exe -omnetPath %OMNET_PATH% -afdxPath %AFDX_PATH% 
-PostProcessor.exe -iPath %AFDX_PATH%\simulations\results -oPath %REPORT_PATH% -oFile %REPORT_NAME%
+python PreProcessor.py -i %XLSX_PATH%\%XLSX_NAME% -o %AFDX_PATH%\afdx\simulations
+python SimProcessor.py -omnetPath %OMNET_PATH% -afdxPath %AFDX_PATH%
+python PostProcessor.py -iPath %AFDX_PATH%\afdx\simulations\results -oPath %REPORT_PATH% -oFile %REPORT_NAME%
 PAUSE
