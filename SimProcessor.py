51f7f1bd17e38fe5e75aec3094d16d17364f315e
badapplexx <ipek.peskirci@gmail.com>
2022-06-04 14:31:25 +0300

    Bug fix and removal redundant lines
    The sequence remained from omnet 5-7 is removed. A new code segment is added to fix a problem: When there is another mingw is already running, (omnetpp is open for example) simprocessor wasn't able to be closed. It is fixed with additional controls.

--- SimProcessor.py
+++ SimProcessor.py
@@ -51,95 +51,26 @@ if __name__ == "__main__":
         f.write(s)
         f.write('echo finish ...\n')
 
+        # count minttys in process before running
+    running_bash_old = 0
+    for proc in psutil.process_iter():
+        if "bash" in proc.name():
+            running_bash_old += 1
     # call simulation
     subprocess.call(f"{mingw_filename} {script_file_name}")
 
+    # count minttys in process after running and wait for termination of new mintty
     running = True
-    print("Waiting for 'bash.exe' to finish ", end = "", flush = True)
+    print("Waiting for 'bash.exe' to finish ", end="", flush=True)
     while running:
         time.sleep(1)
-        print(".", end = "", flush = True)
-        running = False
+        print(".", end="", flush=True)
+        running_bash_new = 0
         for proc in psutil.process_iter():
             if "bash" in proc.name():
-                running = True
-                break
+                running_bash_new += 1
+        if running_bash_new <= running_bash_old:
+            running = False
+
     print("")
-    print("'mintty' finished")
-
-
-if __name__ == "__main_v1__":
-
-    myParser = argparse.ArgumentParser(description="Runs AFDX simulation")
-
-    # Add the arguments
-    myParser.add_argument('-omnetPath',
-                          metavar="<input path>",
-                          type=str,
-                          help='Location of the omnetpp installation',
-                          required=True)
-    myParser.add_argument('-afdxPath',
-                          metavar="<Output path>",
-                          type=str,
-                          help='Location of the AFDX model (project)',
-                          required=True)
-
-    args = myParser.parse_args()
-    omnetpp_path = args.omnetPath
-    afdx_model_path = args.afdxPath
-
-    mingw_filename = omnetpp_path+"\\mingwenv.cmd"
-    mingw_ancat_filename = omnetpp_path+"\\mingwenv_ancat.cmd"
-    mingw_ancat_command_line = 'call "%HOME%\\tools\\win64\\msys2_shell.cmd" -mingw64'
-
-    sim_exe_path = afdx_model_path + "\\src"
-    sim_exe_name = "AFDX-Master-git.exe"
-    sim_ini_path = afdx_model_path + "\\simulations"
-    sim_ini_name = "AutoNetwork.ini"
-    sim_exe_flags = "-m -u Cmdenv -c General -n ../src "
-
-    script_path = sim_ini_path
-    script_file_name = script_path + "\\ancat_run.sh"
-
-    # write script file to feed to the mingw
-    with open(script_file_name, 'w') as f:
-        f.write('echo start ...\n')
-        s = 'cd ' + sim_ini_path + '\n'
-        s = s.replace("C:\\", "/c/").replace("\\", "/")
-        f.write(s)
-        s = f'PATH={omnetpp_path}\\bin:\\opt\\mingw64\\bin:"$PATH" '
-        s += f'{sim_exe_path}\\{sim_exe_name} {sim_exe_flags} '
-        s += f'{sim_ini_path}\\{sim_ini_name}\n'
-        s = s.replace("C:\\", "/c/").replace("\\", "/")
-        f.write(s)
-        f.write('echo finish ...\n')
-
-    # prepare mingw start script
-    file = open(mingw_filename, "r")
-    new_content = ""
-    for line in file:
-        if mingw_ancat_command_line in line:
-            line = line.strip()
-            new_content += line +" " + script_file_name + "\n"
-        else:
-            new_content += line
-    file.close()
-    file = open(mingw_ancat_filename, "w")
-    file.write(new_content)
-    file.close()
-
-    # call simulation
-    subprocess.call(mingw_ancat_filename)
-
-    running = True
-    print("Waiting for 'mintty' to finish ", end = "", flush = True)
-    while running:
-        time.sleep(1)
-        print(".", end = "", flush = True)
-        running = False
-        for proc in psutil.process_iter():
-            if "mintty" in proc.name():
-                running = True
-                break
-    print("")
-    print("'mintty' finished")
+    print("'bash' finished")
